4d582bf1972713156c03fab5d4af062f6bb0b2fb
William Lee <leewp8@gmail.com>
2022-03-08 17:12:59 -0500

    added error

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,14 +6,22 @@ const URL = 'http://localhost:9000/api/todos'
 export default class App extends React.Component {
   state = {
     todos: [],
+    error: '',
+    todoNameInput: '',
   }
+
+  onTodoNameInputChange = evt => {
+    const { value } = evt.target
+    this.setState({ ...this.state, todoNameInput: value})
+  }
+
   fetchAllTodos = () => {
     axios.get(URL)
       .then(res => {
         this.setState({ ...this.state, todos: res.data.data})
       })
       .catch(err => {
-        debugger
+        this.setState({ ...this.state, error: err.response.data.message})
       })
   }
   componentDidMount() {
@@ -23,7 +31,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <div id="error">Error: No error here</div>
+        <div id="error">{this.state.error}</div>
         <div id="todos">
           <h2>To-do's:</h2>
           {
@@ -33,7 +41,7 @@ export default class App extends React.Component {
           }
         </div>
         <form id="todoForm">
-          <input type='text' placeholder='Type to-do'></input>
+          <input value={this.state.todoNameInput} onChange={this.onTodoNameInputChange} type='text' placeholder='Type to-do'></input>
           <input type='submit'></input>
           <button>Clear Completed</button>
         </form>
